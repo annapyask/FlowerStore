b22618f9c0eedddfc3008c5bba823277a6f0fae9
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-10-09 15:23:05 +0300

    change classes

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,30 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-
-
+@Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Flower {
-    @Getter
     private double sepalLength;
     private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
     private FlowerType flowerType;
 
+    public Flower(Flower flower) {
+        this.color = flower.color;
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+    }
+
     public String getColor() {
-        return color.toString();
+        return color.getCode();
     }
 }
--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -1,18 +1,14 @@
 package flower.store;
 
+import lombok.Getter;
+
+@Getter
 public enum FlowerColor {
-    /**
-     * Flower colors.
-     */
-    RED("#FF0000"), BLUE("#0000FF");
-    private final String stringRepresentation;
 
-    FlowerColor(String stringRepresentation) {
-        this.stringRepresentation = stringRepresentation;
-    }
+    RED("#FF0000"), WHITE("#FFFFFF"), YELLOW("#FFFF00");
+    private String code;
 
-    @Override
-    public String toString() {
-        return stringRepresentation;
+    FlowerColor(String code) {
+        this.code = code;
     }
 }
